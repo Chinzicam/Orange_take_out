9c9dacf344e710868f09c6a8652d8de0f909b599
Chinzicam <a3189797316@163.com>
2023-06-28 14:18:21 +0800

    backend complete

--- src/main/java/com/czc/OrangeTakeOutApplication.java
+++ src/main/java/com/czc/OrangeTakeOutApplication.java
@@ -1,15 +1,18 @@
 package com.czc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.czc.*.mapper")
+@Slf4j
 public class OrangeTakeOutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrangeTakeOutApplication.class, args);
+        log.info("项目启动成功...");
     }
 
 }
