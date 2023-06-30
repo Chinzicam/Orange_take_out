b0e402cbd47c18300962c1935d9b2a0385852cd3
Chinzicam <a3189797316@163.com>
2023-06-30 13:54:40 +0800

    全局异常处理

---
+++ src/main/java/com/czc/common/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.czc.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
+
+import static com.sun.jmx.snmp.ThreadContext.contains;
+
+/**
+ * 全局异常处理
+ * @author czc
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    //专门处理SQLIntegrityConstraintViolationException异常
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error(ex.getMessage());
+        //错误日志：Duplicate entry 'czc' for key 'employee.idx_username'
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            String username=split[2];
+            return Result.error("用户名"+username+"已存在");
+        }
+            return Result.error("未知错误");
+    }
+}
