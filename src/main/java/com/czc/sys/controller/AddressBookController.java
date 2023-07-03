6565a04d14d8767fd82c3e0cdf6b3b1f502f2821
Chinzicam <a3189797316@163.com>
2023-07-03 13:20:42 +0800

    :sparkles: 手机端显示菜品，显示收货地址，编辑收货地址

--- src/main/java/com/czc/sys/controller/AddressBookController.java
+++ src/main/java/com/czc/sys/controller/AddressBookController.java
@@ -1,7 +1,15 @@
 package com.czc.sys.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.stereotype.Controller;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.czc.common.BaseContext;
+import com.czc.common.Result;
+import com.czc.sys.entity.AddressBook;
+import com.czc.sys.service.IAddressBookService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,8 +19,40 @@ import org.springframework.stereotype.Controller;
  * @author czc
  * @since 2023-06-28
  */
-@Controller
+@RestController
+@Slf4j
 @RequestMapping("/addressBook")
 public class AddressBookController {
+    @Autowired
+    private IAddressBookService addressBookService;
+
+    /**
+     * 显示收货地址
+     * @param addressBook
+     * @return
+     */
+    @GetMapping("/list")
+    public Result<List<AddressBook>> list(AddressBook addressBook){
+        addressBook.setUserId(BaseContext.getCurrentId());
+        LambdaQueryWrapper<AddressBook> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(AddressBook::getId,addressBook.getUserId());
+        wrapper.orderByDesc(AddressBook::getUpdateTime);
+        List<AddressBook> list = addressBookService.list(wrapper);
+        log.info("list:{}", list);
+        return Result.success(list);
+    }
+
+    /**
+     * 新增收货地址
+     * @param addressBook
+     * @return
+     */
+    @PostMapping
+    public Result<AddressBook> add(@RequestBody AddressBook addressBook){
+        addressBook.setUserId(BaseContext.getCurrentId());
+        log.info("addressBook:{}", addressBook);
+        addressBookService.save(addressBook);
+        return Result.success(addressBook);
+    }
 
 }
--- src/main/java/com/czc/sys/controller/ShoppingCartController.java
+++ src/main/java/com/czc/sys/controller/ShoppingCartController.java
@@ -1,7 +1,19 @@
 package com.czc.sys.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.czc.common.BaseContext;
+import com.czc.common.Result;
+import com.czc.sys.entity.ShoppingCart;
+import com.czc.sys.service.ICategoryService;
+import com.czc.sys.service.IShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -11,8 +23,20 @@ import org.springframework.stereotype.Controller;
  * @author czc
  * @since 2023-06-28
  */
-@Controller
+@RestController
+@Slf4j
 @RequestMapping("/shoppingCart")
 public class ShoppingCartController {
 
+    @Autowired
+    private IShoppingCartService shoppingCartService;
+
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list(){
+        LambdaQueryWrapper<ShoppingCart> wrapper = new LambdaQueryWrapper<>();
+        Long id= BaseContext.getCurrentId();
+        wrapper.eq(ShoppingCart::getId,id);
+        List<ShoppingCart> list = shoppingCartService.list();
+        return Result.success(list);
+    }
 }
