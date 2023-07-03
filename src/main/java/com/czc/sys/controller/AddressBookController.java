304f9b38e511f094051ee1e5c4051ee9d693dc29
Chinzicam <a3189797316@163.com>
2023-07-03 13:32:05 +0800

    :sparkles: 设置默认地址，通过判断IsDefault实现

--- src/main/java/com/czc/sys/controller/AddressBookController.java
+++ src/main/java/com/czc/sys/controller/AddressBookController.java
@@ -1,6 +1,7 @@
 package com.czc.sys.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.czc.common.BaseContext;
 import com.czc.common.Result;
 import com.czc.sys.entity.AddressBook;
@@ -33,10 +34,12 @@ public class AddressBookController {
      */
     @GetMapping("/list")
     public Result<List<AddressBook>> list(AddressBook addressBook){
+        //获取当前用户id
         addressBook.setUserId(BaseContext.getCurrentId());
         LambdaQueryWrapper<AddressBook> wrapper = new LambdaQueryWrapper<>();
         wrapper.eq(AddressBook::getUserId,addressBook.getUserId());
         wrapper.orderByDesc(AddressBook::getUpdateTime);
+
         List<AddressBook> list = addressBookService.list(wrapper);
         log.info("list:{}", list);
         return Result.success(list);
@@ -52,6 +55,25 @@ public class AddressBookController {
         addressBook.setUserId(BaseContext.getCurrentId());
         log.info("addressBook:{}", addressBook);
         addressBookService.save(addressBook);
+        return Result.success(addressBook);
+    }
+
+    /**
+     * 设置默认地址
+     * @param addressBook
+     * @return
+     */
+    @PutMapping("/default")
+    public Result<AddressBook> setDefaultAddress(@RequestBody AddressBook addressBook) {
+        addressBook.setUserId(BaseContext.getCurrentId());
+        //查询setIsDefault为1的用户，改为0
+        LambdaUpdateWrapper<AddressBook> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(AddressBook::getIsDefault,1);
+        wrapper.set(AddressBook::getIsDefault, 0);
+        addressBookService.update(wrapper);
+        //当前用户setIsDefault为1
+        addressBook.setIsDefault(true);
+        addressBookService.updateById(addressBook);
         return Result.success(addressBook);
     }
 
